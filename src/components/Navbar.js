581f5cb4c5c7fcd1533323d85b4e36f134c7d29a
Kudu <raymondreddington40@gmail.com>
2023-05-14 14:47:30 +0530

    Navbar component first

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='text-white'>
+    <h1 className='w-full text-3xl font-extrabold text-[#c8f9e9]'>
+        PRINCIPLES
+    </h1>
+    </div>
+  )
+}
+
+export default Navbar
